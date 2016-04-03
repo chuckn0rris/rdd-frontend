21075d3e01e662c388bc63d1e9fe63b050058889
Alexey Lazarev <siralexlazarev@gmail.com>
2016-04-03 21:18:56 +0700

    edit profile validations

--- classic/src/view/owner/EditProfile.js
+++ classic/src/view/owner/EditProfile.js
@@ -20,225 +20,230 @@ Ext.define('Rdd.view.owner.EditProfile', {
     resizable: false,
 
     items: [{
-        xtype: 'container',
-        layout: 'hbox',
-        width: '100%',
+        xtype: 'form',
         items: [{
             xtype: 'container',
-            layout: 'vbox',
-            marign: 10,
+            layout: 'hbox',
+            width: '100%',
             items: [{
-                xtype: 'image',
-                margin: 10,
-                cls: 'rdd-owner-editform-avatar',
-                height: 120,
-                width: 120,
-                bind: {
-                    src: '{avatar}'
-                }
-            }, {
-                xtype: 'form',
-                width: 120,
-                margin: 10,
-                layout: {
-                    type: 'hbox',
-                    align: 'stretch'
-                },
-                height: 33,
+                xtype: 'container',
+                layout: 'vbox',
+                marign: 10,
                 items: [{
-                    xtype: 'filefield',
-                    name: 'photo',
-                    flex: 1,
-                    buttonConfig: {
-                        width: 120
-                    },
-                    hideLabel: true,
-                    buttonOnly: true,
-                    msgTarget: 'side',
-                    buttonText: 'Load Photo',
-                    listeners: {
-                        change: 'onLoadPhotoFormChange'
+                    xtype: 'image',
+                    margin: 10,
+                    cls: 'rdd-owner-editform-avatar',
+                    height: 120,
+                    width: 120,
+                    bind: {
+                        src: '{avatar}'
                     }
-                }]
-            }, {
-                xtype: 'button',
-                margin: '0 10',
-                text: 'Remove Photo',
-                width: 120,
-                handler: 'removeAvatarImage'
-            }]
-        }, {
-            xtype: 'fieldset',
-            title: 'Main info',
-            flex: 1,
-            margin: '0 10 10 10',
-            layout: 'vbox',
-            defaults: {
-                xtype: 'textfield',
-                width: '100%',
-                allowBlank: false
-            },
-            items: [{
-                fieldLabel: 'First Name',
-                name: 'firstName',
-                bind: '{firstName}'
-            }, {
-                fieldLabel: 'Last Name',
-                name: 'lastName',
-                bind: '{lastName}'
-            }, {
-                fieldLabel: 'Company',
-                name: 'company',
-                bind: '{company}',
-                allowBlank: true
-            }, {
-                xtype: 'fieldcontainer',
-                layout: 'hbox',
-                width: '100%',
-                items: [{
-                    xtype: 'textfield',
-                    width: '85%',
-                    fieldLabel: 'Web Link',
-                    name: 'link',
-                    bind: '{link}'
+                }, {
+                    xtype: 'form',
+                    width: 120,
+                    margin: 10,
+                    layout: {
+                        type: 'hbox',
+                        align: 'stretch'
+                    },
+                    height: 33,
+                    items: [{
+                        xtype: 'filefield',
+                        name: 'photo',
+                        flex: 1,
+                        buttonConfig: {
+                            width: 120
+                        },
+                        hideLabel: true,
+                        buttonOnly: true,
+                        msgTarget: 'side',
+                        buttonText: 'Load Photo',
+                        listeners: {
+                            change: 'onLoadPhotoFormChange'
+                        }
+                    }]
                 }, {
                     xtype: 'button',
-                    text: 'Check',
-                    width: '15%',
-                    handler: 'checkLinkAvailable'
+                    margin: '0 10',
+                    text: 'Remove Photo',
+                    width: 120,
+                    handler: 'removeAvatarImage'
+                }]
+            }, {
+                xtype: 'fieldset',
+                title: 'Main info',
+                flex: 1,
+                margin: '0 10 10 10',
+                layout: 'vbox',
+                defaults: {
+                    xtype: 'textfield',
+                    width: '100%',
+                    allowBlank: false
+                },
+                items: [{
+                    fieldLabel: 'First Name',
+                    name: 'firstName',
+                    bind: '{firstName}'
+                }, {
+                    fieldLabel: 'Last Name',
+                    name: 'lastName',
+                    bind: '{lastName}'
+                }, {
+                    fieldLabel: 'Company',
+                    name: 'company',
+                    bind: '{company}'
+                }, {
+                    xtype: 'fieldcontainer',
+                    layout: 'hbox',
+                    width: '100%',
+                    items: [{
+                        xtype: 'textfield',
+                        width: '85%',
+                        fieldLabel: 'Web Link',
+                        name: 'link',
+                        bind: '{link}'
+                    }, {
+                        xtype: 'button',
+                        text: 'Check',
+                        width: '15%',
+                        handler: 'checkLinkAvailable'
+                    }]
                 }]
             }]
-        }]
-    }, {
-        xtype: 'container',
-        layout: 'hbox',
-        width: '100%',
-        items: [{
-            xtype: 'fieldset',
-            title: 'Location',
-            height: '100%',
-            margin: '0 10 10 10',
-            flex: 1,
-            defaults: {
-                xtype: 'textfield',
-                width: '100%',
-                allowBlank: false
-            },
-            items: [{
-                fieldLabel: 'Country',
-                readOnly: true,
-                name: 'country',
-                value: 'Thailand'
-            }, {
-                xtype: 'combobox',
-                fieldLabel: 'City',
-                name: 'city',
-                bind: '{city}',
-                valueField: 'id',
-                displayField: 'name',
-                store: {
-                    fields: ['id', 'name'],
-                    data: [{
-                        id: 'bangkok',
-                        name: 'Bangkok'
-                    }, {
-                        id: 'pattaya',
-                        name: 'Pattaya'
-                    }, {
-                        id: 'phuket',
-                        name: 'Phuket'
-                    }, {
-                        id: 'krabi',
-                        name: 'Krabi'
-                    }]
-                }
-            }, {
-                fieldLabel: 'District',
-                name: 'district',
-                bind: '{district}'
-            }, {
-                xtype: 'panel',
-                height: 250,
-                width: '100%',
-                border: true,
-                html: '<center><div style="padding-top:20px"><h3>Here will be a map soon...</h3></div></center>'
-            }]
         }, {
-            xtype: 'fieldset',
-            title: 'Contacts',
-            flex: 1,
-            layout: 'vbox',
-            margin: '0 10 10 0',
-            defaults: {
-                xtype: 'textfield',
-                width: '100%',
-                labelWidth: 30,
-                labelSeparator: ''
-            },
+            xtype: 'container',
+            layout: 'hbox',
+            width: '100%',
             items: [{
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/phone.png' />",
-                name: 'phone',
-                bind: {
-                    value: '{phone}'
-                }
+                xtype: 'fieldset',
+                title: 'Location',
+                height: '100%',
+                margin: '0 10 10 10',
+                flex: 1,
+                defaults: {
+                    xtype: 'textfield',
+                    width: '100%',
+                    allowBlank: false,
+                    labelWidth: 70
+                },
+                items: [{
+                    fieldLabel: 'Country',
+                    readOnly: true,
+                    name: 'country',
+                    value: 'Thailand'
+                }, {
+                    xtype: 'combobox',
+                    fieldLabel: 'City',
+                    name: 'city',
+                    bind: '{city}',
+                    valueField: 'id',
+                    displayField: 'name',
+                    store: {
+                        fields: ['id', 'name'],
+                        data: [{
+                            id: 'bangkok',
+                            name: 'Bangkok'
+                        }, {
+                            id: 'pattaya',
+                            name: 'Pattaya'
+                        }, {
+                            id: 'phuket',
+                            name: 'Phuket'
+                        }, {
+                            id: 'krabi',
+                            name: 'Krabi'
+                        }]
+                    }
+                }, {
+                    fieldLabel: 'District',
+                    name: 'district',
+                    bind: '{district}'
+                }, {
+                    fieldLabel: 'Address 1',
+                    name: 'address1',
+                    bind: '{address1}'
+                }, {
+                    fieldLabel: 'Address 2',
+                    name: 'address2',
+                    bind: '{address2}'
+                }]
             }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/mobile.png' />",
-                name: 'mobilePhone',
-                bind: {
-                    value: '{mobilePhone}'
-                }
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/email.png' />",
-                name: 'publicEmail',
-                vtype: 'email',
-                bind: {
-                    value: '{publicEmail}'
-                }
-            }, {
-                xtype: 'container',
-                html: '<br/>',
-                width: '100%'
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/whatsapp.png' />",
-                name: 'whatsapp',
-                bind: {
-                    value: '{socialContacts.whatsapp}'
-                }
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/viber.png' />",
-                name: 'viber',
-                bind: {
-                    value: '{socialContacts.viber}'
-                }
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/facebook.png' />",
-                name: 'facebook',
-                bind: {
-                    value: '{socialContacts.facebook}'
-                }
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/skype.png' />",
-                name: 'skype',
-                bind: {
-                    value: '{socialContacts.skype}'
-                }
-            }, {
-                fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/instagram.png' />",
-                name: 'instagram',
-                bind: {
-                    value: '{socialContacts.instagram}'
-                }
+                xtype: 'fieldset',
+                title: 'Contacts',
+                flex: 1,
+                layout: 'vbox',
+                margin: '0 10 10 0',
+                defaults: {
+                    xtype: 'textfield',
+                    width: '100%',
+                    labelWidth: 30,
+                    labelSeparator: ''
+                },
+                items: [{
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/phone.png' />",
+                    name: 'phone',
+                    bind: {
+                        value: '{phone}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/mobile.png' />",
+                    name: 'mobilePhone',
+                    bind: {
+                        value: '{mobilePhone}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/email.png' />",
+                    name: 'publicEmail',
+                    vtype: 'email',
+                    bind: {
+                        value: '{publicEmail}'
+                    }
+                }, {
+                    xtype: 'container',
+                    html: '<br/>',
+                    width: '100%'
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/whatsapp.png' />",
+                    name: 'whatsapp',
+                    bind: {
+                        value: '{socialContacts.whatsapp}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/viber.png' />",
+                    name: 'viber',
+                    bind: {
+                        value: '{socialContacts.viber}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/facebook.png' />",
+                    name: 'facebook',
+                    bind: {
+                        value: '{socialContacts.facebook}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/skype.png' />",
+                    name: 'skype',
+                    bind: {
+                        value: '{socialContacts.skype}'
+                    }
+                }, {
+                    fieldLabel: "<img width='24px' height='24px' src='classic/resources/images/instagram.png' />",
+                    name: 'instagram',
+                    bind: {
+                        value: '{socialContacts.instagram}'
+                    }
+                }]
             }]
+        }],
+        buttons: [{
+            text: 'Save',
+            formBind: true,
+            width: 100,
+            handler: 'saveProfileChanges'
+        }, {
+            text: 'Close',
+            width: 100,
+            handler: 'closeWindow'
         }]
-    }],
-
-    buttons: [{
-        text: 'Save',
-        width: 100,
-        handler: 'saveProfileChanges'
-    }, {
-        text: 'Close',
-        width: 100,
-        handler: 'closeWindow'
     }]
 });
